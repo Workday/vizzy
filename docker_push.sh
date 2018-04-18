7aeae397c022d53a094af1e8bdc20b283a112e32
Scott Bishop <scottbishop70@gmail.com>
2018-04-17 19:02:56 -0700

    Remove div container from forms, they were being added twice. Tag Docker image as latest on Travis CI

--- docker_push.sh
+++ docker_push.sh
@@ -2,3 +2,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 export REPO=$DOCKER_USERNAME/vizzy
 docker push $REPO:$TRAVIS_COMMIT
+docker tag $REPO:$TRAVIS_COMMIT $REPO:latest
+docker push $DOCKER_USERNAME/vizzy:latest
