9f2e416d51b63271bb2c22a9c4bbaa51ad37a3a6
Scott Bishop <scottbishop70@gmail.com>
2018-10-23 10:47:33 -0700

    Update older k8s deployment files, increase timeout

--- public/upload_images_to_server.rb
+++ public/upload_images_to_server.rb
@@ -193,12 +193,10 @@ def poll_for_commit(build_id, images_uploaded)
   puts 'Waiting for commit to finalize'
   sleep 2 # Sleep 2 before the first check so we can catch the 'simple' commits that have no diffs quickly
 
-  # floor: 60 tries * 10 seconds == 10 minutes
-  max_tries = 60
-  # scaling as needed for when there are more than 60 images, 10 seconds per image
-  max_tries = images_uploaded if images_uploaded > max_tries
-  # ceiling 360 * 10 seconds = 3600 seconds = 1 hour
-  max_tries = 360 if images_uploaded > 360
+  # floor: 360 tries * 10 seconds == 1 hour
+  max_tries = 360
+  # scaling as needed
+  max_tries = max_tries + images_uploaded
 
   time_in_seconds = max_tries * 10
   time_in_minutes = time_in_seconds / 60
