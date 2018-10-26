5e5e40e8a256ee64062896be0772a606ab8e9044
Scott Bishop <scottbishop70@gmail.com>
2018-10-26 09:49:10 -0700

    Update deployment version

--- k8s/deploy-with-env-vars.sh
+++ k8s/deploy-with-env-vars.sh
@@ -9,4 +9,4 @@ RAILS_ENV=production
 RUN_TESTS=false
 DOCKER_REGISTRY=docker-dev-artifactory.workday.com
 
-./deploy-vizzy.sh --api-server=$API_SERVER --rails-env=$RAILS_ENV --vizzy-version=v3.3.3 --namespace=$NAMESPACE --vizzy-uri=$VIZZY_URI --replica-pods=$REPLICA_PODS --memory=$MEMORY --docker-registry=$DOCKER_REGISTRY --run-tests=$RUN_TESTS
+./deploy-vizzy.sh --api-server=$API_SERVER --rails-env=$RAILS_ENV --vizzy-version=v3.3.6 --namespace=$NAMESPACE --vizzy-uri=$VIZZY_URI --replica-pods=$REPLICA_PODS --memory=$MEMORY --docker-registry=$DOCKER_REGISTRY --run-tests=$RUN_TESTS
